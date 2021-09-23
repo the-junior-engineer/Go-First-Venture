ae7aae4bdfa8bef053a4ad2ffdbc4dbae8b95111
sravindra05 <91222037+the-junior-engineer@users.noreply.github.com>
2021-09-23 21:29:36 +0100

    Update variables.go

--- variables/variables.go
+++ variables/variables.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	/*
+	Variables are elements of a program which hold a certain value that may or may not change during the course of its execution
+	This function illustrates how to create variables
 		Variable declaration:
 			1) Shorthand declaration (declaration + assignment):
 				This cannot be used outside function bodies.
@@ -13,9 +15,10 @@ func main() {
 				example: var x = 42
 						 var z int = 75
 	*/
-	fmt.Println("Hello There!")
+	
 	x := 45 // Short hand declaration of variables
 	fmt.Println(x + 55)
+
 	var z string = "This is a string!" // The var keyword in action
 	fmt.Println(z)
 }
